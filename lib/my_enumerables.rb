148d417cedfdb48eca899a328cb3cdc3daeea931
MateoLCS <mateusz.namysl0318@gmail.com>
2023-10-08 23:39:47 +0200

    Implemented array methods in order to improve my block knowledge

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,101 @@ end
 # to this method
 class Array
   # Define my_each here
+
+  def my_each
+    unless block_given? then return self end
+
+    for i in 0...self.length
+      yield self[i]
+    end
+      self
+  end
+
+  def my_each_with_index
+    unless block_given? then return self end
+
+    for i in 0...self.length
+      yield self[i], i
+    end
+      self
+  end
+
+  def my_all?
+    unless block_given? then return self end
+
+    for i in 0...self.length
+      if yield(self[i]) != true
+        return false
+      end
+    end
+    true
+  end
+
+  def my_any?
+    unless block_given? then return self end
+
+      for i in 0...self.length
+        if yield(self[i]) == true
+          return true
+        end
+      end
+      false
+  end
+
+  def my_none?
+    unless block_given? then return self end
+
+    for i in 0...self.length
+      if yield(self[i]) == true
+        return false
+      end
+    end
+      true
+  end
+
+  def my_count
+    unless block_given? then return self.length end
+
+    counter = 0
+
+    for i in 0...self.length
+      if yield(self[i]) == true
+        counter += 1
+      end
+    end
+    counter
+  end
+
+  def my_map
+    unless block_given? then return self end
+        
+      res_array = []
+  
+      for i in 0...self.length
+        res_array << yield(self[i])
+      end
+      res_array
+  end
+  
+  def my_select
+    unless block_given? then return self end
+
+    res_array = []
+
+    for i in 0...self.length
+      if yield(self[i]) == true
+        res_array << self[i]
+      end
+    end
+    res_array
+  end
+
+  def my_inject(start_value)
+    acc = start_value
+
+    for i in 0...self.length
+      acc = yield(acc, self[i])
+    end
+    acc
+  end
 end
